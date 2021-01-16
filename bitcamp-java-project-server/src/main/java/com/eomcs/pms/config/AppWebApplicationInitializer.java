6006c0584075448be134c67b8a60b71f6dc2f620
eunchae-cho <dmsco8981@gmail.com>
2021-01-16 21:52:42 +0900

    ,

--- bitcamp-java-project-server/src/main/java/com/eomcs/pms/config/AppWebApplicationInitializer.java
+++ bitcamp-java-project-server/src/main/java/com/eomcs/pms/config/AppWebApplicationInitializer.java
@@ -1,41 +1,40 @@
 package com.eomcs.pms.config;
 
 import javax.servlet.Filter;
-
 import org.springframework.web.filter.CharacterEncodingFilter;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
-public class AppWebApplicationInitializer 
-  extends AbstractAnnotationConfigDispatcherServletInitializer {
+public class AppWebApplicationInitializer
+extends AbstractAnnotationConfigDispatcherServletInitializer {
 
   @Override
   protected Class<?>[] getRootConfigClasses() {
-	  // ContextLoaderListener가 사용할 Java Config 클래스 리턴
-    return new Class<?>[] {RootConfig.class, MybatisConfig.class};
+    // ContextLoaderListener 가 사용할 java config 클래스를 리턴한다.
+    return new Class<?>[] {RootConfig.class, DatabaseConfig.class, MybatisConfig.class};
   }
-  
+
   @Override
   protected Class<?>[] getServletConfigClasses() {
-	  // DispatscherServlet이 사용할 Java Config 클래스 리턴
-	  return new Class<?>[] {AppWebConfig.class};
+    // DispatcherServlet 이 사용할 java config 클래스를 리턴한다.
+    return new Class<?>[] {AppWebConfig.class};
   }
-  
+
   @Override
   protected String[] getServletMappings() {
     return new String[] {"/app/*"};
   }
-  
+
   @Override
   protected String getServletName() {
     return "app";
   }
-  
+
   @Override
-	protected Filter[] getServletFilters() {
-		return new Filter[] {
-			new CharacterEncodingFilter("UTF-8")
-		};
-	}
+  protected Filter[] getServletFilters() {
+    return new Filter[] {
+        new CharacterEncodingFilter("UTF-8")
+    };
+  }
 }
 
 
--- bitcamp-java-project-server/src/main/java/com/eomcs/pms/config/RootConfig.java
+++ bitcamp-java-project-server/src/main/java/com/eomcs/pms/config/RootConfig.java
@@ -4,10 +4,10 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ComponentScan.Filter;
 import org.springframework.context.annotation.FilterType;
 
-// Spring webmvc framework에서 ContextLoaderListener가 사용할 Java Config이다.
-// 웹 컨포넌트가 공유할 객체를 선언
+// Spring WebMVC 프레임워크에서 ContextLoaderListener 가 사용할 Java Config 이다.
+// 웹 컴포넌트가 공유할 객체를 선언한다.
 // 예) DAO, Service 등
-
+//
 @ComponentScan(
     value="com.eomcs.pms",
     excludeFilters = {
